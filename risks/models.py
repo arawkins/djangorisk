a2f9d75ea4f3e92b823e44c8dcb9fdd6e112f76b
Alan Rawkins <arawkins@gmail.com>
2017-12-12 15:50:58 -0800

    changed get_json to get_data

--- risks/models.py
+++ risks/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# make separate riskdata, and relate field models to that
-# make basic Risk have just a normal meta field relation to denote structure
-
 class Risk(models.Model):
     """ A class to model risks.
 
@@ -12,12 +9,18 @@ class Risk(models.Model):
     """
 
     name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-    def get_json(self):
-        """ Returns a json representation of this risk. """
+    def get_data(self):
+        """ Returns a dictionary representation of this risk.
+
+        Fields that are related to this risk are gathered and
+        added to a fields list in the dictionary.
+        """
 
         data = {
             'id': self.id,
@@ -26,26 +29,27 @@ class Risk(models.Model):
         }
 
         for field in self.textfields.all():
-            data['fields'].append(field.get_json())
+            data['fields'].append(field.get_data())
 
         for field in self.numberfields.all():
-            data['fields'].append(field.get_json())
+            data['fields'].append(field.get_data())
 
         for field in self.datetimefields.all():
-            data['fields'].append(field.get_json())
+            data['fields'].append(field.get_data())
 
         for field in self.enumfields.all():
-            data['fields'].append(field.get_json())
+            data['fields'].append(field.get_data())
 
         return data
 
 class RiskField(models.Model):
-    """ An abstract class to base our different risk fields off of.
+    """ An abstract base class to base our different risk fields off of.
 
-    Includes constants, a common name field, and a boilerplate
-    get_json method that will be overridden by child classes.
-    ForeignKey relationships to the Risk are defined in the child
-    classes.
+    Includes constants, common model field, and a boilerplate
+    get_data method that will be overridden by child classes.
+    Since each child class is responsible for a specific data type,
+    ForeignKey relationships to Risks are defined in the child
+    class, as are value fields, for holding data.
     """
 
     # Type constants
@@ -59,12 +63,17 @@ class RiskField(models.Model):
     def __str__(self):
         return self.name
 
-    def get_json(self):
-        """ Returns a JSON representation of this field """
+    def get_data(self):
+        """ Returns a dictionary representation of this field
+
+        type and possible_values are defined in child classes
+        """
 
         return {
             'id'  : self.id,
-            'name': self.name
+            'name': self.name,
+            'type': None,
+            'possible_values': None
         }
 
     class Meta:
@@ -73,53 +82,50 @@ class RiskField(models.Model):
 class RiskTextField(RiskField):
     """ A field to hold text data on risks """
     value = models.TextField(blank=True, null=True)
-    risk = models.ForeignKey(Risk, on_delete=models.CASCADE, related_name='textfields')
+    risk = models.ForeignKey('Risk', on_delete=models.CASCADE, related_name='textfields')
 
-    def get_json(self):
-        data = super(RiskTextField, self).get_json()
+    def get_data(self):
+        data = super(RiskTextField, self).get_data()
         data['type'] = RiskField.TEXT
         return data
 
 class RiskNumberField(RiskField):
     """ A field to hold numerical values on a risk. """
     value = models.DecimalField(blank=True, null=True, max_digits=24, decimal_places=2)
-    risk = models.ForeignKey(Risk, on_delete=models.CASCADE, related_name='numberfields')
+    risk = models.ForeignKey('Risk', on_delete=models.CASCADE, related_name='numberfields')
 
-    def get_json(self):
-        data = super(RiskNumberField, self).get_json()
+    def get_data(self):
+        data = super(RiskNumberField, self).get_data()
         data['type'] = RiskField.NUMBER
         return data
 
 class RiskDateTimeField(RiskField):
     """ A field to hold Datetime information on risks """
     value = models.DateTimeField(blank=True, null=True)
-    risk = models.ForeignKey(Risk, on_delete=models.CASCADE, related_name='datetimefields')
+    risk = models.ForeignKey('Risk', on_delete=models.CASCADE, related_name='datetimefields')
 
-    def get_json(self):
-        data = super(RiskDateTimeField, self).get_json()
+    def get_data(self):
+        data = super(RiskDateTimeField, self).get_data()
         data['type'] = RiskField.DATETIME
         return data
 
 
 class RiskEnumField(RiskField):
-    """ A field to hold text data on a risk.
+    """ A field to hold text data on a risk, with a defined set of possible values.
 
-    Has a defined set of possible values,
-    stored as a pipe delimited string.
+    Possible values are stored as a pipe delimited string.
     """
 
     value = models.CharField(blank=True, null=True, max_length=128)
-    risk = models.ForeignKey(Risk, on_delete=models.CASCADE, related_name='enumfields')
-
-    # possible_values should be a pipe delimted string of possible values for this field
+    risk = models.ForeignKey('Risk', on_delete=models.CASCADE, related_name='enumfields')
     possible_values = models.TextField(blank=True, null=True)
 
-    def get_json(self):
-        data = super(RiskEnumField, self).get_json()
+    def get_data(self):
+        data = super(RiskEnumField, self).get_data()
         data['type'] = RiskField.ENUM
         data['possible_values'] = self.get_list_of_possible_values()
         return data
 
     def get_list_of_possible_values(self):
-        """ Splits the pipe delimited possible_values string into a list """
+        """ Returns possible_values string as a list """
         return [value.strip() for value in self.possible_values.split('|')]
--- risks/views.py
+++ risks/views.py
@@ -7,12 +7,12 @@ from .models import Risk
 @require_safe
 def index(request):
     response_data = []
-    risks = Risk.objects.all().prefetch_related('textfields', 'numberfields', 'datetimefields')
+    risks = Risk.objects.all().prefetch_related('textfields', 'numberfields', 'datetimefields', 'enumfields')
     for risk in risks:
-        response_data.append(risk.get_json())
+        response_data.append(risk.get_data())
     return JsonResponse(response_data, safe=False)
 
 @require_safe
 def show(request, risk_id):
     risk = get_object_or_404(Risk, pk=risk_id)
-    return JsonResponse(risk.get_json())
+    return JsonResponse(risk.get_data())
