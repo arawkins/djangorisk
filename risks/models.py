a10e754e94d6a32553b6bfb1f93fb55f036b7a9f
Alan Rawkins <arawkins@gmail.com>
2017-12-12 17:25:06 -0800

    Added response field tests

--- risks/models.py
+++ risks/models.py
@@ -60,7 +60,7 @@ class RiskField(models.Model):
     ENUM = 'enum'
 
     name = models.CharField(max_length=200)
-
+    
     def __str__(self):
         return self.name
 
--- risks/tests.py
+++ risks/tests.py
@@ -1,40 +1,65 @@
 import json
 from django.test import TestCase, Client
-from .models import Risk, RiskTextField, RiskEnumField, RiskDateTimeField, RiskNumberField
+from .models import Risk, RiskField, RiskTextField, RiskEnumField, RiskDateTimeField, RiskNumberField
 
 class TestEndPointStatusCodes(TestCase):
     """ Tests the status codes of the endpoint URLs in the Risks app """
 
+    def setUp(self):
+        self.client = Client()
+
     def test_risks(self):
         """ Does /risks/ return status of 200? """
-        c = Client()
-        response = c.get('/risks/')
+        response = self.client.get('/risks/')
         self.assertEqual(response.status_code, 200)
 
     def test_risk_url(self):
         """ If we add one Risk, does /risks/1/ return a status of 200? """
         risk = Risk.objects.create(name="Boats")
-        c = Client()
-        response = c.get('/risks/1/')
+        response = self.client.get('/risks/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_risk_url_404(self):
         """ If we don't add a risk, does /risks/1/ return a status of 404? """
-        c = Client()
-        response = c.get('/risks/1/')
+        response = self.client.get('/risks/1/')
         self.assertEqual(response.status_code, 404)
 
 class TestEndPointData(TestCase):
     """ Verifies the data returned from the API Endpoints """
 
     def setUp(self):
+        # Set up data
         r1 = Risk.objects.create(name='Dinosaurs')
         RiskTextField.objects.create(name='Species', risk=r1)
         RiskDateTimeField.objects.create(name='First found', risk=r1)
         RiskEnumField.objects.create(name='Diet', possible_values='Carnivore|Herbivore|Omnivore', risk=r1)
         RiskNumberField.objects.create(name='Number of legs', risk=r1)
 
-    def test_data_length(self):
+        # make a request and save the response for testing
         c = Client()
-        response = c.get('/risks/1/')
-        self.assertEquals(len([response.json()]),1)
+        self.response = c.get('/risks/1/').json()
+
+    def test_data_length(self):
+        self.assertEquals(len([self.response]),1)
+
+    def test_risk_name(self):
+        self.assertEquals(self.response['name'], 'Dinosaurs')
+
+    def test_risk_field_count(self):
+        self.assertEquals(len(self.response['fields']), 4)
+
+    def test_risk_text_field(self):
+        expected_data = {'id': 1, 'name': 'Species', 'type': RiskField.TEXT}
+        self.assertEquals(self.response['fields'][0], expected_data)
+
+    def test_risk_number_field(self):
+        expected_data = {'id': 1, 'name': 'Number of legs', 'type': RiskField.NUMBER}
+        self.assertEquals(self.response['fields'][1], expected_data)
+
+    def test_risk_datetime_field(self):
+        expected_data = {'id': 1, 'name': 'First found', 'type': RiskField.DATETIME}
+        self.assertEquals(self.response['fields'][2], expected_data)
+
+    def test_risk_enum_field(self):
+        expected_data = {'id': 1, 'name': 'Diet', 'type': RiskField.ENUM, 'possible_values': ['Carnivore', 'Herbivore', 'Omnivore']}
+        self.assertEquals(self.response['fields'][3], expected_data)
